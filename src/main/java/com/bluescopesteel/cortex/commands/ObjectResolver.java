94c2bc59d3ba007e8d6b2be0b61b35feb3a1a643
maj890 <maj890@eis-407009.adeis.uow.edu.au>
2014-09-29 09:48:03 +1000

    Moved object resolution into one spot to avoid multiple declarations

---
+++ src/main/java/com/bluescopesteel/cortex/commands/ObjectResolver.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bluescopesteel.cortex.commands;
+
+import com.bluescopesteel.cortex.InternalVariable;
+
+/**
+ *
+ * @author maj890
+ */
+public class ObjectResolver {
+    public static Object resolveObjectReference(String objectReference) throws Throwable {
+        Command command = new CommandParser().parseCommand(objectReference);
+        Object object = command.execute();
+        if (object instanceof InternalVariable) {
+            return ((InternalVariable) object).getVariableValue();
+        }
+        return object;
+    }
+}
