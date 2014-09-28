15579494f9d1872cf8452b710e74887f46a4353b
maj890 <maj890@eis-407009.adeis.uow.edu.au>
2014-09-29 09:48:15 +1000

    Added field setting

--- src/main/java/com/bluescopesteel/cortex/commands/SetValueCommand.java
+++ src/main/java/com/bluescopesteel/cortex/commands/SetValueCommand.java
@@ -3,16 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.bluescopesteel.cortex.commands;
 
 import com.bluescopesteel.cortex.InternalVariable;
+import java.lang.reflect.Field;
 
 /**
  *
  * @author Mitchell Just (Mitchell.Just@BlueScopeSteel.com)
  */
-public class SetValueCommand implements Command{
+public class SetValueCommand implements Command {
+
     private Object target;
     private Object value;
 
@@ -21,28 +22,37 @@ public class SetValueCommand implements Command{
         this.value = value;
     }
 
-    
     @Override
     public Object execute() throws Throwable {
-        System.out.println("Executing Set Value Command");
-        if(target instanceof Command) {
-            System.out.println("Resolving Target...");
+        // System.out.println("Executing Set Value Command");
+        if (!(target instanceof FieldAccessCommand) && target instanceof Command) {
+            // System.out.println("Resolving Target...");
+            target = ((Command) target).execute();
+        }
+        if (value instanceof Command) {
+            // System.out.println("Resolving Value...");
+            value = ((Command) value).execute();
+        }
+
+        if (target instanceof InternalVariable) {
+            // System.out.println("Internal Variable Setting");
+            ((InternalVariable) target).setVariableValue(value);
+        } else if (target instanceof FieldAccessCommand) {
+            System.out.println("Setting a field");
+            setFieldValue((FieldAccessCommand) target, value);
             target = ((Command)target).execute();
-        }
-        if(value instanceof Command) {
-            System.out.println("Resolving Value...");
-            value = ((Command)value).execute();
-        }
-        
-        
-        if(target instanceof InternalVariable) {
-            System.out.println("Internal Variable Setting");
-            ((InternalVariable)target).setVariableValue(value);
-        }
-        else {
+        } else {
             System.out.println("Not Internal! Don't know how to do this yet! :(");
         }
 
-        return(target);
+        return (target);
     }
+
+    public void setFieldValue(FieldAccessCommand fac, Object value) throws Throwable {
+        Object object = ObjectResolver.resolveObjectReference(fac.getObjectName());
+        Field field = object.getClass().getField(fac.getMemberName());
+        field.set(object, value);
+    }
+
+    
 }
